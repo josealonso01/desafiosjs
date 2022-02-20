0815d3d52cfc089e05e6ef68dcf2c2c1564c9fd6
Josefina Alonso <joseealonsoo01@gmail.com>
2022-02-20 14:04:00 -0300

    agregue ciclo con for

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ const calculateDiscount = price => price * 0.9
 
 result = calculateDiscount(price)
 alert("El resultado con 10% de descuento es: " + result)
+
+
+
+for (let i = 0; i < 1000 ; i++) {
+    const res = price;
+
+    if (price >= 1000) {
+        alert('tenes envio gratis')
+        break;
+    }
+
+    if (price < 1000){
+        alert('costo de envio sujeto a CorreoArgentino')
+        break;
+    }
+    
+}
+
